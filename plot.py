b61479b39fc6c5b2939d775c042a535b1837f4ef
Marco Trevisiol <marco.trevisiol@deimosholding.com>
2025-04-14 15:09:38 +0200

    Reworked plot so that it reads from stdin

--- plot.py
+++ plot.py
@@ -1,29 +1,24 @@
+import sys
+
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-i", type=str, default="output.dat", help="name of the input file")
-# many more arguments
+parser.add_argument("-i", "--input", type=str, default="-", help="name of the input file ('-' for stdin)")
 
 arguments = parser.parse_args()
-filename = arguments.i
 
-print(f"Plot of the following file: {arguments.i}.")
-
-
-# Load the data, skipping the first line (which starts with '#')
-data = np.loadtxt(filename, delimiter=",", comments="#")
+filename = arguments.input if arguments.input != '-' else sys.stdin
+data = np.loadtxt(filename, delimiter=",")
 
 # Extract time and magnetization
 time = data[:, 0]
 magnetization = data[:, 1]
-#aligned_neurons = data[:, 2] / 1000
 
 # Plot
 plt.figure(figsize=(8, 5))
-plt.plot(time, magnetization, marker='o', linestyle='-', color='blue')
-#plt.plot(time, aligned_neurons, label="Aligned Neurons", color="green", linestyle='--')
+plt.plot(time, magnetization, linestyle='-', color='blue')
 plt.title("Magnetization vs Time")
 plt.xlabel("Time Step")
 plt.ylabel("Magnetization")
