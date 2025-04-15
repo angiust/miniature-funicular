f6f36ef800c741b48af1745ae7ca088bb90438dc
angiust <angelo.giustiniani@gmail.com>
2025-04-15 18:55:11 +0200

    refactoring plot.py

--- plot.py
+++ plot.py
@@ -8,16 +8,27 @@ import argparse
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-i", "--input", type=str, default="-", help="name of the input file ('-' for stdin)")
+parser.add_argument("--title", type=str, default="", help="title of the plot")
 
 arguments = parser.parse_args()
 
 filename = arguments.input if arguments.input != '-' else sys.stdin
-data = np.loadtxt(filename)
+data = np.loadtxt(filename, delimiter=",")
 
 # Extract time and magnetization
 time = np.arange(data.shape[0])
-magnetization = data
+average_magnetization = data[:, 0]
+std_deviation = data[:, 1]
 
+plt.figure(figsize=(8, 5))
+plt.errorbar(time, average_magnetization, yerr=std_deviation, color='blue', ecolor='lightgray', elinewidth=1, capsize=3)
+plt.title(f"Magnetization vs Time {arguments.title}")
+plt.xlabel("Time Step")
+plt.ylabel("Magnetization")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+"""
 # Plot
 plt.figure(figsize=(8, 5))
 plt.plot(time, magnetization, linestyle='-', color='blue')
@@ -27,3 +38,4 @@ plt.ylabel("Magnetization")
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+"""
