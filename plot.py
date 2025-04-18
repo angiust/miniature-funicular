c6a9f419b62fdec8971531a518ebe0d13a29ec15
angiust <angelo.giustiniani@gmail.com>
2025-04-18 10:59:56 +0200

    make impossible states un-representable: fixed plot.py

--- plot.py
+++ plot.py
@@ -9,8 +9,7 @@ import argparse
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-i", "--input", type=str, default="-", help="name of the input file ('-' for stdin)")
 parser.add_argument("--title", type=str, default="", help="custom plot title")
-parser.add_argument("--mode", type=str, choices=["show", "savefig"], default="show", help="whether to show or save the plot")
-parser.add_argument("--output", type=str, default="plot.png", help="output filename if saving the plot")
+parser.add_argument("--output", type=str, help="output filename if saving the plot")
 
 arguments = parser.parse_args()
 
@@ -21,25 +20,6 @@ data = np.loadtxt(filename, delimiter=",")
 time = np.arange(data.shape[0])
 average_magnetization = data[:, 0]
 std_deviation = data[:, 1]
-"""
-plt.figure(figsize=(8, 5))
-plt.errorbar(time, average_magnetization, yerr=std_deviation, color='blue', ecolor='lightgray', elinewidth=1, capsize=3)
-plt.title(f"Magnetization vs Time {arguments.title}")
-plt.xlabel("Time Step")
-plt.ylabel("Magnetization")
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-# Plot
-plt.figure(figsize=(8, 5))
-plt.plot(time, magnetization, linestyle='-', color='blue')
-plt.title("Magnetization vs Time")
-plt.xlabel("Time Step")
-plt.ylabel("Magnetization")
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-"""
 
 plt.figure(figsize=(8, 5))
 plt.plot(time, average_magnetization, label='Magnetization', color='blue')
@@ -51,7 +31,7 @@ plt.grid(True)
 plt.legend()
 plt.tight_layout()
 
-if arguments.mode == "show":
-    plt.show()
-else:
+if arguments.output:
     plt.savefig(arguments.output)
+else:
+    plt.show()
